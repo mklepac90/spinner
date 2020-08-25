ac337dbf28a444691db4f79af29698be928eb377
Michael Klepac <mklepac90@gmail.com>
2020-08-25 19:21:43 +0000

    even more fun with timeout!

---
+++ spinner2.js
@@ -0,0 +1,47 @@
+// process.stdout.write('hello from spinner1.js... \rheyyy\n');
+const spinner = '|/-\|/-\|'
+let timer = 100;
+
+for (const char of spinner) {
+  setTimeout(() => {
+    process.stdout.write(`\r${char} `)
+  }, timer);
+  timer += 200;
+}
+
+
+// setTimeout(() => {
+//   process.stdout.write('\r|   ');
+// }, 100);
+
+// setTimeout(() => {
+//   process.stdout.write('\r/   ');
+// }, 300);
+
+// setTimeout(() => {
+//   process.stdout.write('\r-   ');
+// }, 500);
+
+// setTimeout(() => {
+//   process.stdout.write('\r\\   '); 
+// }, 700);
+
+// setTimeout(() => {
+//   process.stdout.write('\r|   '); 
+// }, 900);
+
+// setTimeout(() => {
+//   process.stdout.write('\r/   '); 
+// }, 1100);
+
+// setTimeout(() => {
+//   process.stdout.write('\r-   '); 
+// }, 1300);
+
+// setTimeout(() => {
+//   process.stdout.write('\r\\   '); 
+// }, 1500);
+
+// setTimeout(() => {
+//   process.stdout.write('\r|   '); 
+// }, 1700);
